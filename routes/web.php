54d11ec2c606b95330e5c6c5993cdd6803238f79
ayoub jkr <jakouriayoub08@gmail.com>
2025-11-28 16:39:57 +0100

    create Chirper Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+// Route::get('/', function () {
+//     return view('home');
+// })->name('home');
+
+Route::resource('/', [ChirperController::class, 'index'] );
