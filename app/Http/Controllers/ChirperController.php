4089349b38f6a0fcd83f15abb478d5be66c15e69
ayoub jkr <jakouriayoub08@gmail.com>
2025-11-28 16:45:10 +0100

    create Home page

--- app/Http/Controllers/ChirperController.php
+++ app/Http/Controllers/ChirperController.php
@@ -12,7 +12,21 @@ class ChirperController extends Controller
     public function index()
     {
         $shirps = [
-            
+            [
+                'author' => 'Jane Doe',
+                'message' => 'Just deployed my first Laravel app! 🚀',
+                'time' => '5 minutes ago'
+            ],
+            [
+                'author' => 'John Smith',
+                'message' => 'Laravel makes web development fun again!',
+                'time' => '1 hour ago'
+            ],
+            [
+                'author' => 'Alice Johnson',
+                'message' => 'Working on something cool with Chirper...',
+                'time' => '3 hours ago'
+            ]
         ];
 
         return view('home', compact('ships'));
