dcdc3157330b01a1de215ae809baaaa0c90d3406
ayoub jkr <jakouriayoub08@gmail.com>
2025-11-30 12:12:54 +0100

    Create the Edit View

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 
-Route::resource('chirps', ChirpController::class)
-    ->only(['store', 'edit', 'update', 'destroy']);
+// Route::resource('chirps', ChirpController::class)
+//     ->only(['store', 'edit', 'update', 'destroy']);
