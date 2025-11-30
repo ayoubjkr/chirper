a6ba7a99d67d6b1efc321fbcfa7971d6c52bb96c
ayoub jkr <jakouriayoub08@gmail.com>
2025-11-30 12:11:30 +0100

    add add resource route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::post('/chirps', [ChirpController::class, 'store'])->name('store');
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
