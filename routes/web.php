26bb7634b1adad7efebd90e17ef45a8bd3287cc1
ayoub jkr <jakouriayoub08@gmail.com>
2025-11-30 12:09:22 +0100

    add routes for edit, update, and delete

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index'] )->name('home');
 Route::post('/chirps', [ChirpController::class, 'store'])->name('store');
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
